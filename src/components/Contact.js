6cca36e7fe847882f1b517b027745760d609618e
vickycode88 <vickypd2003@gmail.com>
2025-10-26 12:31:10 +0530

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,8 +5,8 @@ function Contact() {
     <div className="container mt-5 text-center">
       <h2>Contact</h2>
       <p>Feel free to reach out!</p>
-      <p><strong>Email:</strong> youremail@example.com</p>
-      <p><strong>LinkedIn:</strong> linkedin.com/in/yourprofile</p>
+      <p><strong>Email:</strong> vickypd2003@gmail.com.com</p>
+      <p><strong>LinkedIn:</strong> linkedin.com/in/vicky2003</p>
     </div>
   );
 }
