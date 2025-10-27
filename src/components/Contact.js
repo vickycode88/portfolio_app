52e78c90a40f81b22cf3c1a7a82f9a4ed47114a5
vickycode88 <vickypd2003@gmail.com>
2025-10-27 22:02:51 +0530

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,7 @@ function Contact() {
       <h2>Contact</h2>
       <p>Feel free to reach out!</p>
       <p><strong>Email:</strong> vickypd2003@gmail.com</p>
-      <p><strong>LinkedIn:</strong> linkedin.com/in/vicky2003</p>
+      <p><strong>LinkedIn:</strong> https://www.linkedin.com/in/vicky-das-159774330/</p>
     </div>
   );
 }
